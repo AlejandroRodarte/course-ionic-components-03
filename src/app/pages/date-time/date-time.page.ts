40a8f38f6b11273e5a09c1aadec2a709c0ffa1cc
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-03-14 22:23:36 -0600

    49., 50. ion-datetime

--- src/app/pages/date-time/date-time.page.ts
+++ src/app/pages/date-time/date-time.page.ts
@@ -9,6 +9,27 @@ export class DateTimePage implements OnInit {
 
   fechaNacimiento = new Date();
 
+  customPickerOptions = {
+    buttons: [
+      {
+        text: 'Save',
+        handler: (e) => {
+          console.log('Clicked Save!');
+          console.log(e);
+        }
+      },
+      {
+        text: 'Log',
+        handler: () => {
+          console.log('Clicked Log. Do not dismiss');
+          return false;
+        }
+      }
+    ]
+  };
+
+  customDate = new Date();
+
   constructor() { }
 
   ngOnInit() {
