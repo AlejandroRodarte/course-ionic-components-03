3d580b915328c8c2cc258c88e9893505b492ab35
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-03-15 21:03:49 -0600

    56. ion-input

---
+++ src/app/pages/input/input.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.page.html',
+  styleUrls: ['./input.page.scss'],
+})
+export class InputPage implements OnInit {
+
+  public nombre: string;
+
+  public usuario = {
+    email: '',
+    password: ''
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(form: NgForm): void {
+    console.log(form);
+    console.log(this.usuario);
+    console.log('Formulario subido');
+  }
+
+}
