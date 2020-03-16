9986b6dad6563f79f93ed6e56ccfa8f58bc6db4c
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-03-15 21:42:27 -0600

    59. ion-item-sliding

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { User } from './../../models/user';
 import { Observable } from 'rxjs';
+import { IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -9,6 +10,9 @@ import { Observable } from 'rxjs';
   styleUrls: ['./list.page.scss'],
 })
 export class ListPage implements OnInit {
+
+  @ViewChild('lista', { static: false })
+  public lista: IonList;
 
   public usuarios: Observable<User>;
 
@@ -18,6 +22,21 @@ export class ListPage implements OnInit {
 
   ngOnInit() {
     this.usuarios = this.dataService.getUsers();
+  }
+
+  favorite(usuario: User) {
+    console.log('favorite', usuario);
+    this.lista.closeSlidingItems();
+  }
+
+  share(usuario: User) {
+    console.log('share', usuario);
+    this.lista.closeSlidingItems();
+  }
+
+  unread(usuario: User) {
+    console.log('unread', usuario);
+    this.lista.closeSlidingItems();
   }
 
 }
