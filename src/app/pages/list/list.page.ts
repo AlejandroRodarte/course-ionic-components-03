f54d4f61ce21347cdd5b3ec8f895d9ab19fc6e13
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-03-17 19:56:35 -0600

    76. ion-toast

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { User } from './../../models/user';
 import { Observable } from 'rxjs';
-import { IonList } from '@ionic/angular';
+import { IonList, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -17,7 +17,8 @@ export class ListPage implements OnInit {
   public usuarios: Observable<User>;
 
   constructor(
-    private dataService: DataService
+    private dataService: DataService,
+    private toastController: ToastController
   ) { }
 
   ngOnInit() {
@@ -27,16 +28,30 @@ export class ListPage implements OnInit {
   favorite(usuario: User) {
     console.log('favorite', usuario);
     this.lista.closeSlidingItems();
+    this.presentToast('Se guardo en favoritos');
   }
 
   share(usuario: User) {
     console.log('share', usuario);
     this.lista.closeSlidingItems();
+    this.presentToast('Compartido perro');
   }
 
   unread(usuario: User) {
     console.log('unread', usuario);
     this.lista.closeSlidingItems();
+    this.presentToast('Borrado papu');
+  }
+
+  async presentToast(message: string) {
+
+    const toast = await this.toastController.create({
+      message,
+      duration: 2000
+    });
+
+    await toast.present();
+
   }
 
 }
